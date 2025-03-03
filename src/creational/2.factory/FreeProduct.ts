55b5e97280d3665eb46e67fcd91314cc276d7512
daomtthuan <thuandmt@outlook.com>
2025-03-04 04:54:53 +0700

    Factory

--- src/creational/2.factory/FreeProduct.ts
+++ src/creational/2.factory/FreeProduct.ts
@@ -4,10 +4,7 @@ import type { IProduct } from './IProduct.js';
 
 /** Free product. */
 export class FreeProduct implements IProduct {
-  /**
-   * @param name Product name.
-   * @param price Product price.
-   */
+  /** @inheritdoc */
   public constructor(
     public name: string,
     public price: number,
