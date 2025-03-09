6a8e507b2045ccc83d57c0e977af038fc3dcba35
daomtthuan <thuandmt@outlook.com>
2025-03-09 15:21:16 +0700

    Factory

--- src/creational/2.factory/index.ts
+++ src/creational/2.factory/index.ts
@@ -3,10 +3,14 @@ import { ProductFactory } from './ProductFactory.js';
 const factory = new ProductFactory();
 
 const product1 = factory.create('Product 1', 0);
-console.log(product1, '\n');
+console.log(product1);
+
+console.log();
 
 const product2 = factory.create('Product 2', 100);
-console.log(product2, '\n');
+console.log(product2);
+
+console.log();
 
 const product3 = factory.create('Product 3', 200);
-console.log(product3, '\n');
+console.log(product3);
