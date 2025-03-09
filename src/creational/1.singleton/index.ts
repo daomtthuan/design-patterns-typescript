af8818381e8578afef63b63e5fcb1133689be4c2
daomtthuan <thuandmt@outlook.com>
2025-03-09 14:46:49 +0700

    Singleton

--- src/creational/1.singleton/index.ts
+++ src/creational/1.singleton/index.ts
@@ -2,10 +2,7 @@ import { Calculator } from './Calculator.js';
 import { Store } from './Storage.js';
 
 const result1 = Calculator.getInstance().sum(1, 2);
-Store.getInstance().add(result1);
-
 const result2 = Calculator.getInstance().sum(3, 4);
-Store.getInstance().add(result2);
 
-const result3 = Calculator.getInstance().sum(5, 6);
-Store.getInstance().add(result3);
+Store.getInstance().add(result1);
+Store.getInstance().add(result2);
