1ff479a6dea9fced57a8393eded83cf726c973a7
daomtthuan <thuandmt@outlook.com>
2025-03-09 21:17:30 +0700

    Factory Method

--- src/creational/3.factory-method/factories/CarFactory.ts
+++ src/creational/3.factory-method/factories/CarFactory.ts
@@ -1,3 +1,5 @@
+import colors from 'picocolors';
+
 import type { IVehicle } from '../objects/IVehicle.js';
 import type { IVehicleFactory } from './IVehicleFactory.js';
 
@@ -5,6 +7,11 @@ import { Car } from '../objects/Car.js';
 
 /** Car factory. */
 export class CarFactory implements IVehicleFactory {
+  /** @inheritdoc */
+  public constructor() {
+    console.log(colors.gray('CarFactory instance created'));
+  }
+
   /** @inheritdoc */
   public create(name: string, maxSpeed: number): IVehicle {
     return new Car(`[Car] ${name}`, maxSpeed, 10_000);
--- src/creational/3.factory-method/factories/PlaneFactory.ts
+++ src/creational/3.factory-method/factories/PlaneFactory.ts
@@ -1,3 +1,5 @@
+import colors from 'picocolors';
+
 import type { IVehicle } from '../objects/IVehicle.js';
 import type { IVehicleFactory } from './IVehicleFactory.js';
 
@@ -5,6 +7,11 @@ import { Plane } from '../objects/Plane.js';
 
 /** Plane factory. */
 export class PlaneFactory implements IVehicleFactory {
+  /** @inheritdoc */
+  public constructor() {
+    console.log(colors.gray('PlaneFactory instance created'));
+  }
+
   /** @inheritdoc */
   public create(name: string, maxSpeed: number): IVehicle {
     return new Plane(`[Plane] ${name}`, maxSpeed, 1_000_000_000);
