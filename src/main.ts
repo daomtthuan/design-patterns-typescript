03f55e85f8162b3fcbc8889091d0631ab6a97128
daomtthuan <thuandmt@outlook.com>
2025-03-09 21:04:44 +0700

    Setup

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,28 @@ import url from 'url';
 
 const [name] = process.argv.slice(2);
 if (!name) {
-  throw new Error('Please input the name of the design pattern');
+  throw new Error('Please input the name of the Design pattern');
 }
 
-for await (const groupName of ['creational', 'structural', 'behavioral']) {
+let entryPath;
+for (const groupName of ['creational', 'structural', 'behavioral']) {
   const groupPath = path.resolve(import.meta.dirname, groupName);
   if (!fs.existsSync(groupPath)) {
     continue;
   }
 
-  const patternDirName = fs.readdirSync(groupPath).find((dirName) => dirName.endsWith(name));
+  const patternDirName = fs
+    .readdirSync(groupPath)
+    .find((dirName) => new RegExp(`^${name}\\..+$`).test(dirName) || new RegExp(`^\\d+\\.${name}$`).test(dirName));
   if (!patternDirName) {
     continue;
   }
 
-  const entryPath = path.resolve(groupPath, patternDirName, 'index.ts');
-  await import(url.pathToFileURL(entryPath).href);
+  entryPath = path.resolve(groupPath, patternDirName, 'index.ts');
 }
+
+if (!entryPath) {
+  throw new Error('Design pattern not found');
+}
+
+await import(url.pathToFileURL(entryPath).href);
